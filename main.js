ee7bfe7b15b5eace4aa28a70338498811fe08b4b
Nafeesmerchand <160695676+Nafeesmerchand@users.noreply.github.com>
2024-03-07 00:22:17 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+// Array
+// Array for string
+//let Children: String [] = ["Nafees", "Shees", "Shayayn", "Shaheer", "Museera", "Fabiha"]
+//console.log(Children[3], Children[4], Children[5]);
+// Array for any type
+//let Student: any [] = ["Nafees", "Shees", "Shayayn", "Shaheer", "Museera", "Fabiha", 122, 333, true];
+//console.log(Student[4], Student[6], Student[8]);
+// Array (we can change value or item)
+var furniture = ["Table", "chair", "Cupboard"];
+furniture[1] = "Pen";
+console.log(furniture);
+// Pascal case
+var my_office_city = "Karachi";
+console.log(my_office_city);
---
+++ main.ts
@@ -0,0 +1,22 @@
+// Array
+
+// Array for string
+
+let Children: String [] = ["Nafees", "Shees", "Shayayn", "Shaheer", "Museera", "Fabiha"]
+console.log(Children[3], Children[4], Children[5]);
+
+//Array for any type
+let Student: any [] = ["Nafees", "Shees", "Shayayn", "Shaheer", "Museera", "Fabiha", 122, 333, true];
+console.log(Student[4], Student[6], Student[8]);
+
+// Array (we can change value or item)
+
+let furniture: String [] = ["Table", "chair", "Cupboard"];
+furniture[1] = "Pen";
+console.log(furniture)
+
+// Pascal case
+
+let my_office_cit: String = "Karachi"
+console.log(my_office_cit);
+
